097e660bda58cdaf391444e1d76b44c8a7a78442
Ryan Y <ryoung@brandeis.edu>
2016-08-14 19:34:55 -0400

    About to make a combine segments method.

---
+++ GLM/combineSegments.m
@@ -0,0 +1,5 @@
+% Purpose: Function takes all of the different data an interleaves them.
+% This is critical for a final post-processing before GLMing all subject
+% data.
+function combineSegments()
+end
